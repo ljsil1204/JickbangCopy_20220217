dee84ff94d57f8c2c7160670e0f097e15dd4a1cb
이진실 <ljsil1204@naver.com>
2022-02-17 15:17:36 +0900

    - 방 목록 클래스 추가 - RoomAdapter 클래스 생성   - AttayAdapter 상속   - 생성자 수정 > 부모클래스에세 넘겨줄 자료 받아오기 - 어댑터 클래스 getView 기초작업   - room_list_item의 모양이 그대로 나오도록

---
+++ app/src/main/java/com/neppplus/jickbangcopy_20220217/adapters/RoomAdapter.kt
@@ -0,0 +1,31 @@
+package com.neppplus.jickbangcopy_20220217.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.neppplus.jickbangcopy_20220217.R
+import com.neppplus.jickbangcopy_20220217.datas.RoomData
+
+class RoomAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<RoomData>
+
+) : ArrayAdapter<RoomData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
+
+}
---
+++ app/src/main/java/com/neppplus/jickbangcopy_20220217/datas/RoomData.kt
@@ -0,0 +1,9 @@
+package com.neppplus.jickbangcopy_20220217.datas
+
+class RoomData (
+    val price : Int,
+    val address : String,
+    val floor : Int,
+    val description : String,
+) {
+}
